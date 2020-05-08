b5fad8040e2f74d526cabd4e233aeca73f53c356
t20x <mpx@gmail.com>
2020-05-08 23:11:33 +0100

    asdf

---
+++ inline/B.cpp
@@ -0,0 +1,9 @@
+static inline int foo()
+{
+    return 1;
+}
+
+void boo()
+{
+    foo();
+}
---
+++ string/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    const char* s = "whateveraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    std::cout << s;
+    //s[1] = 1;
+    return 0;
+}
+
