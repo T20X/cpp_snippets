b24cdf3c7a066f92ef32f82a51e2981132872ce8
t20x <mpx@gmail.com>
2020-09-06 23:00:30 +0000

    fix the build

---
+++ folly/io_demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <utility>
+#include <iostream>
+#include <folly/Format.h>
+#include <folly/futures/Future.h>
+#include <folly/executors/ThreadedExecutor.h>
+#include <folly/Uri.h>
+#include <folly/FBString.h>
+
+int main()
+{
+    return 0;
+}
